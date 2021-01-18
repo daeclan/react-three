9703f9d44bfc36ed84290e3cc8343b8aa283a72e
Declan Murphy <declxn@gmail.com>
2021-01-18 16:19:03 -0600

    Three cubes spinning, using destructuring and props to add different instances of the same thing.

--- react-three-cubes/src/App.js
+++ react-three-cubes/src/App.js
@@ -3,14 +3,14 @@ import './App.scss';
 
 import { Canvas, useFrame } from "react-three-fiber";
 
-const Box = () => {
+const SpinningCube = ({ position }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01)
   )
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={1, 1, 1} />
-      <meshStandardMaterial attach='material' />
+      <meshStandardMaterial attach='material' color='green' />
     </mesh>
   );
 };
@@ -18,8 +18,11 @@ const Box = () => {
 function App() {
   return (
     <>
-      <Canvas>
-        <Box />
+      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 40 }}>
+        <ambientLight intensity={0.6} />
+        <SpinningCube position={[0, 1, 0]} />
+        <SpinningCube position={[-2, 1, -5]} />
+        <SpinningCube position={[5, 1, -2]} />
       </Canvas>
     </>
   );
